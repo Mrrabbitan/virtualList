b576484d48aa2d9e48c3fb9d09cc181cefc22017
anzp <anzp@rd.netease.com>
2021-08-27 11:19:29 +0800

    :sparkles: added new tools

--- components/VirtualList/src/index.tsx
+++ components/VirtualList/src/index.tsx
@@ -1,36 +1,29 @@
 
 
 import React from 'react';
-
-const VirtualTable = () =>{
+import { BaseTable } from 'ali-react-table'
+const VirtualTable = (props) =>{
+    const { loading, dataSource, columns } = props
+    
     return (<>
         <BaseTable
-        {...props}
-        dataSource={props.dataSource}
-        columns={[multiSelect({
-          primaryKey: 'userId',
-          keys: seleKeys,
-          onChange: (keys: string, rows: any, action: any) => {
-            setSeleKeys(keys);
-            rowSelectionOnChange(keys, rows, action);
-            props.rowSelection.onSelect(keys, undefined, rows)
-            props.rowSelection.onSelectAll(keys, undefined, rows)
-          },
-        })({ columns: props.columns, dataSource: props.dataSource }), ...virtualColumn(), virtalManMadeColumn()]}
-        primaryKey="userId"
-        style={{
-          '--header-bgcolor': '#fafafa',
-          '--header-color': 'rgba(0, 0, 0, 0.85)',
-          clear: 'both',
-          width: 'auto',
-          height: length * 63 + 35 > 600 ? 600 : length * 65 + 50,
-          overflow: 'auto',
-        }}
-        isStickyHeader
-        stickyTop={0}
-        useVirtual
-        isLoading={loading}
-      />
+            {...props}
+            dataSource={dataSource}
+            columns={columns}
+            primaryKey="userId"
+            style={{
+            '--header-bgcolor': '#fafafa',
+            '--header-color': 'rgba(0, 0, 0, 0.85)',
+            clear: 'both',
+            width: 'auto',
+            height: length * 63 + 35 > 600 ? 600 : length * 65 + 50,
+            overflow: 'auto',
+            }}
+            isStickyHeader
+            stickyTop={0}
+            useVirtual
+            isLoading={loading}
+        />
     </>)
 }
 
